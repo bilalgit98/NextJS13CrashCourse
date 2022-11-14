26cf0b7182a93f9de649f77d758ba5de800359e4
bilalgit98 <71947506+bilalgit98@users.noreply.github.com>
2022-11-14 19:56:17 +0000

    Server Side Components with async/await to Fetch Todos

--- nextjs-13-demo/app/todos/[todoid]/page.tsx
+++ nextjs-13-demo/app/todos/[todoid]/page.tsx
@@ -1,4 +1,6 @@
 import React from 'react'
+import { Todo } from '../../../typings'
+
 
 type pageProps = {
     params : {
@@ -7,9 +9,25 @@ type pageProps = {
 
 }
 
-function TodoPage({params : {todoid}} : pageProps) {
+const fetchTodo= async (todoid: string ) =>{
+    const response = await fetch(`https://jsonplaceholder.typicode.com/todos/${todoid}`);
+    const todo= await response.json();
+    return todo
+}
+
+
+async function TodoPage({params : {todoid}} : pageProps) {
+    const todo : Todo = await fetchTodo(todoid)
+
+
   return (
-    <div>TodoPage:{todoid} </div>
+    <div className='p-10 bg-yellow-200 border-2 m-2 shadow-lg'> 
+        <p>
+            #{todo.id} : {todo.title}
+        </p>
+        <p > Completed: {todo.completed ? "Yes" : "No"} </p>
+        <p className='border-t border-black mt-5 text-right' >By User : {todo.userId} </p>
+         </div>
   )
 }
 
