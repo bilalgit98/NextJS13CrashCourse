6ba91d38b023aba0d718f2fff10ad9125f167c82
bilalgit98 <71947506+bilalgit98@users.noreply.github.com>
2022-11-15 15:27:40 +0000

    Dynamic Head Name

--- nextjs-13-demo/app/layout.tsx
+++ nextjs-13-demo/app/layout.tsx
@@ -1,18 +1,21 @@
 //activiating tailwindCSS
-import '../styles/globals.css'
-import Header from './Header'
+import "../styles/globals.css";
+import Header from "./Header";
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html>
-      <head />
+      <head>
+        <title>To-Do NextJS 13 Crash Course</title>
+      </head>
       <body>
-        <Header/>
-        {children}</body>
+        <Header />
+        {children}
+      </body>
     </html>
-  )
+  );
 }
